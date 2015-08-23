276b234f4edbb001753d16bc301e93a39c03425f
Stanly Johnson <stanlyjohnson01@gmail.com>
2015-08-23 08:45:03 +0530

    alarm added

--- facedetect.py
+++ facedetect.py
@@ -1,35 +1,70 @@
+import numpy as np
 import cv2
-import sys
+import winsound
 
-cascPath = sys.argv[1]
-faceCascade = cv2.CascadeClassifier(cascPath)
+from video import create_capture
+from common import clock, draw_str
 
-video_capture = cv2.VideoCapture(0)
+help_message = '''
+USAGE: facedetect.py [--cascade <cascade_fn>] [--nested-cascade <cascade_fn>] [<video_source>]
+'''
 
-while True:
-    # Capture frame-by-frame
-    ret, frame = video_capture.read()
+def detect(img, cascade):
+    rects = cascade.detectMultiScale(img, scaleFactor=1.3, minNeighbors=4, minSize=(30, 30), flags = cv2.CASCADE_SCALE_IMAGE)
+    if len(rects) == 0:
+        return []
+    rects[:,2:] += rects[:,:2]
+    return rects
 
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+def draw_rects(img, rects, color):
+    for x1, y1, x2, y2 in rects:
+        cv2.rectangle(img, (x1, y1), (x2, y2), color, 2)
 
-    faces = faceCascade.detectMultiScale(
-        gray,
-        scaleFactor=1.1,
-        minNeighbors=5,
-        minSize=(30, 30),
-        flags=cv2.cv.CV_HAAR_SCALE_IMAGE
-    )
+if __name__ == '__main__':
+    import sys, getopt
+    print help_message
 
-    # Draw a rectangle around the faces
-    for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    args, video_src = getopt.getopt(sys.argv[1:], '', ['cascade=', 'nested-cascade='])
+    try:
+        video_src = video_src[0]
+    except:
+        video_src = 0
+    args = dict(args)
+    cascade_fn = args.get('--cascade', "../../data/haarcascades/haarcascade_frontalface_alt.xml")
+    nested_fn  = args.get('--nested-cascade', "../../data/haarcascades/haarcascade_eye.xml")
 
-    # Display the resulting frame
-    cv2.imshow('Video', frame)
+    cascade = cv2.CascadeClassifier(cascade_fn)
+    nested = cv2.CascadeClassifier(nested_fn)
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+    cam = create_capture(video_src, fallback='synth:bg=../data/lena.jpg:noise=0.05')
 
-# When everything is done, release the capture
-video_capture.release()
-cv2.destroyAllWindows()
+    while True:
+        ret, img = cam.read()
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        gray = cv2.equalizeHist(gray)
+
+        t = clock()
+        rects = detect(gray, cascade)
+        vis = img.copy()
+        draw_rects(vis, rects, (0, 255, 0))
+        if not nested.empty():
+            for x1, y1, x2, y2 in rects:
+                roi = gray[y1:y2, x1:x2]
+                vis_roi = vis[y1:y2, x1:x2]
+                subrects = detect(roi.copy(), nested)
+                draw_rects(vis_roi, subrects, (255, 0, 0))
+
+        else:
+            winsound.Beep(500,1000)
+            print "here"
+            draw_str(vis, (20, 20), 'SLEEP: %.1f ms' % (dt*1000))
+            
+        dt = clock() - t
+        draw_str(vis, (20, 20), 'monitoring: %.1f ms' % (dt*1000))
+        cv2.imshow('facedetect', vis)
+        
+
+        if 0xFF & cv2.waitKey(5) == 27:
+            break
+    
+    cv2.destroyAllWindows()
